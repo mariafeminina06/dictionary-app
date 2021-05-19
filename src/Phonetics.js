c9cda043cd3955db992c887c83facc1629c736b6
Maria Escalante <maria_feminina06@yahoo.com>
2021-05-19 13:33:21 +0200

    added phonetic sound

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,11 +1,21 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetics({ phonetic }) {
+  const audio = new Audio(phonetic.audio);
+  const playSound = (audioFile) => {
+    audioFile.play();
+  };
+
   return (
     <div className="Phonetics">
-      <a href={phonetic.audio} target="_blank" rel="noreferrer">
-        {phonetic.text}
-      </a>
+      <button
+        className="btn btn-outline-dark
+"
+        onClick={() => playSound(audio)}
+      >
+        <i class="fas fa-volume-up sound-icon"></i> {phonetic.text}
+      </button>
     </div>
   );
 }
